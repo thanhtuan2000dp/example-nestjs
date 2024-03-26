eb49c0d1ea5333716b8b66192b16d53a147deb29
TuanNT88 <tuannt88@fpt.com>
2024-03-26 16:30:48 +0700

    auth module

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable({})
-export class AuthService {}
+export class AuthService {
+  signup() {
+    return { msg: 'I am signing up' };
+  }
+
+  signin() {
+    return { msg: 'I am signing in' };
+  }
+}
