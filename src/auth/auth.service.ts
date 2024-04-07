7287038a710ca7ec8f757873bb06efababc5d042
davidtuan-macbook <thanhtuan2000dp@gmail.com>
2024-04-08 00:48:59 +0700

    throw exception when signup exist email

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,44 @@
-import { Injectable } from '@nestjs/common';
+// ** NestJS Imports
+import { ForbiddenException, Injectable } from '@nestjs/common';
+
+// ** Third party Imports
+import * as argon from 'argon2';
+
+// ** File Imports
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable({})
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  signup() {
-    return { msg: 'I am signing up' };
+  async signup(dto: AuthDto) {
+    //Generate the hash password
+    const hash = await argon.hash(dto.password);
+
+    //Save the new user to the Database
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+        select: {
+          id: true,
+          email: true,
+          createdAt: true,
+        },
+      });
+      //Return the saved user
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+      throw error;
+    }
   }
 
   signin() {
