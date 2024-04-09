afceadf815cb20dee4570bec404cc0a75bae16ec
TuanNT88 <tuannt88@fpt.com>
2024-04-09 17:35:22 +0700

    update config nestjs

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  constructor() {
+  constructor(private config: ConfigService) {
     super({
       datasources: {
         db: {
-          url: 'postgresql://davidtuan:123456@localhost:5434/nestdb?schema=public',
+          url: config.get('DATABASE_URL'),
         },
       },
     });
